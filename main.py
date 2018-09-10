f7653bd73ea000d7fc5f9742f9382ad398cd6d13
Bingyin Hu <bingyin.hu@gmail.com>
2018-09-10 11:13:37 -0400

    added jobDir

--- main.py
+++ main.py
@@ -62,10 +62,11 @@ if __name__ == "__main__":
 ##    xsdDir = raw_input("Please type in the directory of the xsd schema file:")
 ##    while xsdDir == "":
 ##        xsdDir = raw_input("Please type in the directory of the xsd schema file:")
-    csvDir = "E:/Dropbox/DIBBS/data_update/info_update_xml/schema/033018to060418.csv"
-    xmlDir = "E:/Dropbox/DIBBS/data_update/info_update_xml/schema/xmlForUpdate2018-06-04/"
-    schemaID = '5abe807ce74a1d06fd9943a0' # PNC_schema_033018
-    xsdDir = "./schema/PNC_schema_060418.xsd"
+    csvDir = "E:/Dropbox/DIBBS/data_update/info_update_xml/schema/072618to081218.csv"
+    xmlDir = "E:/Dropbox/DIBBS/data_update/info_update_xml/schema/forUpload060718_to_072618/"
+    # schemaID = '5b1ebeb9e74a1d61fc43654d' # PNC_schema_060718
+    schemaID = 'xml'
+    xsdDir = "./schema/PNC_schema_081218.xsd"
     (backupDir, csvCompDir) = runcsvCompletor(csvDir, xmlDir, schemaID)
     if csvCompDir is not None and backupDir is not None:
         runInfoUpdate(csvCompDir)
@@ -82,6 +83,6 @@ if __name__ == "__main__":
         print "csv file completion failure! Please check your csv file."
     print "================================================================"
     print "Validation started!"
-    logName = runValidation(backupDir, xsdDir)
+    logName = runValidation(backupDir, xsdDir, '')
     print "================================================================"
 
